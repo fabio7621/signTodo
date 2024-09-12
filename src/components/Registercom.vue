a3a0132d989b02e81f0ab45a8cf0ff7acc2a0975
fabio7621 <d6200121a@gmail.com>
2024-09-12 10:51:57 +0800

    fix

--- src/components/Registercom.vue
+++ src/components/Registercom.vue
@@ -64,7 +64,6 @@ const signup = async () => {
     signOutbox.password = ''
     signOutbox.nickname = ''
     confirmPassword.value = ''
-
     router.push('login')
   } catch (error) {
     alert('註冊失敗')
